6e13b7b666c66ef0af24cea4b0899d000602927e
Allof02 <70244801+Allof02@users.noreply.github.com>
2022-11-15 23:41:49 -0500

    Add files via upload

--- breakout.asm
+++ breakout.asm
@@ -29,7 +29,16 @@ Paddle_length:
 
 .eqv GREEN 0x00ff00
 .eqv BLACK 0x000000
+.eqv BLUE 0x0000ff
+.eqv ORANGE 0xffa500
+.eqv PINK 0xf980d1
+.eqv GREY 0x808080
+.eqv RED 0xff0000
+.eqv WHITE 0xFFFFFF
 .eqv Paddle_length 5
+.eqv multxByte 4
+.eqv multyByte 128
+
 
 
 	
@@ -58,7 +67,7 @@ paddle_right_old:
 ########  BALL  #########
 ball_current_x:
 	.space 4 # x * 4 gives the offset should be added to ADDR_DSPL to locate the current x_axis
-ball_currrent_y:
+ball_current_y:
 	.space 4# y * 128 gives the offset should also be added to ADDR_DSPL to locatetbe current y_axis
 ball_current_speed:
 	.space 4 #During Initialization stage, this should be set to 2 (default speed)
@@ -68,9 +77,15 @@ ball_move_status:
 	.space 4 # During Initialization stage, this should be set to 0 (default status it got changed only when user first press a key)
 ############BLOCK ARRAYs#######
 Left_array:
-	.word 772, 784, 796, 808,820,832,844,856,868,880
+	.word 772, 784, 796, 808,820,832,844,856,868,880, 900, 912, 924, 936, 948, 960, 972, 984, 996, 1008, 1028, 1040,1052,1064,1076,1088,1100,1112,1124, 1136
+	
 Right_array:
 	.word 780, 792, 804, 816,828,840,852,864,876,888
+	.word 908, 920, 932, 944,956968,980,992,1004, 1016
+	.word 1036, 1048, 1060, 1072, 1084, 1096, 1108,1120, 1132, 1144
+
+Hit_counter:
+	.space 120 
 	
 ##############################################################################
 # Code
@@ -82,8 +97,9 @@ Right_array:
 main:
 
 ######################### DRAW INITIALIZATIONS ##############################
+	
     lw $t0, ADDR_DSPL	                                      # $t0 stores the base address for display
-	li $t1, 0xff0000	                                         # $t1 stores the red colour code
+	li $t1, GREY	                                         # $t1 stores the red colour code
 	
 	
 	
@@ -124,7 +140,7 @@ main:
 	
 	li $t1, 0x00ff00 # set to green
 	lw $t0, ADDR_DSPL #reset t0
-	li $t2,  5 #counter
+	li $t2,  6 #counter
 	
 	addi $t0, $t0, 3636 #set t0 to where the original paddle should be (128 * 28 + 4 *13)
 	
@@ -143,15 +159,102 @@ main:
 	sw $t0, 0($t9) #store initial left point of paddle to memory
 	
 	lw $t1, ADDR_DSPL 
-	addi $t1, $t1, 3652
+	addi $t1, $t1, 3656
 	la $t8, paddle_right
 	sw $t1, 0($t8) #store initial right point of paddle to memory
-	
-	
-	
-	
-	
 
+	################$  PAINT  BLOCKS $############
+
+##### BLOCK 1 ######
+
+	li $t2, BLUE #Painter
+	
+#paint_block1:
+#	beq $t1, $t0, end_of_paint_block1
+#	la $t3, Left_array #first address where left_array is stored
+#	add $t3, $t3, $t1 #increment address of left_array to access data coming after
+#	lw $t4, 0($t3) # load offset to t4
+#	lw $t5, ADDR_DSPL #load bitmap addr
+#	add $t5, $t5, $t4 #add offset
+#	sw $t2, 0($t5) #paint
+#	addi $t1, $t1, 4 #increment counter
+#	j paint_block1
+#	end_of_paint_block1:
+
+###FIRST ROW###
+li $t9, BLUE #Painter
+li $t0, 772 #start point 
+li $t1, 888 #end point
+
+paint_block1:
+	bgt $t0, $t1, end_of_draw_block_first_row
+	lw $t2, ADDR_DSPL #bitmap gp
+	add $t2, $t2, $t0 #add offset
+	sw $t9, 0($t2) #paint
+	addi $t0, $t0, 4 #increment by 4 bytes
+	j paint_block1
+	end_of_draw_block_first_row:
+	
+###SECOND ROW###	
+li $t9, ORANGE #Painter
+li $t0, 900 #start point 
+li $t1, 1016 #end point
+
+paint_block2:
+	bgt $t0, $t1, end_of_draw_block_second_row
+	lw $t2, ADDR_DSPL #bitmap gp
+	add $t2, $t2, $t0 #add offset
+	sw $t9, 0($t2) #paint
+	addi $t0, $t0, 4 #increment by 4 bytes
+	j paint_block2
+	end_of_draw_block_second_row:
+
+###THIRD ROW ###
+li $t9, RED #Painter
+li $t0, 1028 #start point 
+li $t1, 1144 #end point
+
+paint_block3:
+	bgt $t0, $t1, end_of_draw_block_third_row
+	lw $t2, ADDR_DSPL #bitmap gp
+	add $t2, $t2, $t0 #add offset
+	sw $t9, 0($t2) #paint
+	addi $t0, $t0, 4 #increment by 4 bytes
+	j paint_block3
+	end_of_draw_block_third_row:
+
+####DRAW THE BALL AND INITIALIZE BALL_VARIABLES####
+#initial: add + 3516
+# x = 15 (15 * 4)
+# y = 27 (27 * 128)
+
+li $a0, 15 #load current_x_axis (since its initialization, we have not put value into current_x_axis yet)
+jal find_ballX_axis_distance # call function : with return value $v0 = 15 * 4
+li $t8, 0
+add $t8, $t8, $v0 #save 15 * 4 into $t8
+
+li $a0, 27 
+jal find_ballY_axis_distance # call function : with return value $v0 = 27 * 128
+li $t6, 0
+add $t6, $t6, $v0 #save 27 * 128 into $t6
+
+add $t8, $t8, $t6 # sum up t6 and t8 to get total bytes (offset)
+
+lw $t0, ADDR_DSPL
+add $t0, $t0, $t8# add
+
+li $t9, WHITE
+sw $t9, 0($t0)
+
+li $t4, 15  # initial x coordinate of ball
+li $t3, 27 #initial y coordinate of ball
+la $t1, ball_current_x
+la $t2, ball_current_y
+sw $t4, 0($t1) #save to current_x
+sw $t3, 0($t2) #save to current_y
+
+
+	
 game_loop:
 
 
@@ -325,3 +428,30 @@ syscall
 
 #5. Go back to 1
     b game_loop
+
+###Functions###
+
+## 4 * X ##
+find_ballX_axis_distance:
+	li $t0, 4
+	li $v0, 0
+	ballx_innerloop:
+		beqz $a0, end_of_ballx_innerloop
+		add $v0, $v0, $t0
+		addi $a0, $a0, -1
+		j ballx_innerloop
+		end_of_ballx_innerloop:
+		jr $ra
+
+##128 * Y ##
+find_ballY_axis_distance:
+	li $t0, 128
+	li $v0, 0
+	bally_innerloop:
+		beqz $a0, end_of_bally_innerloop
+		add $v0, $v0, $t0
+		addi $a0, $a0, -1
+		j bally_innerloop
+		end_of_bally_innerloop:
+		jr $ra
+
